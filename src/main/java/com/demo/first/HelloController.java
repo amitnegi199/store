7c970f82be15fd75218fb1b392b8ffd72899f4c4
amitnegi199 <amit71639@gmail.com>
2026-01-09 20:57:32 +0530

    added negi

--- src/main/java/com/demo/first/HelloController.java
+++ src/main/java/com/demo/first/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController
     @GetMapping("/hello")
     public String sayHello()
     {
-        return "Hello world india negi";
+        return "Hello world indianegi";
     }
 
     //@GetMapping("/user")
